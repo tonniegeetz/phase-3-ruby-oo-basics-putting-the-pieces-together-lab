e6c1c844aa6f598fb8475e2f49fb0561eddf57a0
tonniegeetz <tonniegeetz@gmail.com>
2024-06-17 20:28:59 +0300

    initializee and ting

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
+class Book
+  attr_accessor :title, :author, :page_count, :genre
 
+  def initialize(title, author = 'Unknown', page_count = 0, genre = 'unknown')
+    @title = title
+    @author = author
+    @page_count = page_count
+    @genre = genre
+  end
 
+  def turn_page
+    puts 'Flipping the page...wow, you read fast!'
+  end
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,18 @@
 # Make your shoe class here!
+
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+
+  def initialize(brand, color = 'Unknown', size = 0, material = 'Unknown', condition = 'new')
+    @brand = brand
+    @color = color
+    @size = size
+    @material = material
+    @condition = condition
+  end
+
+  def cobble
+    puts 'Your shoe is as good as new!'
+    @condition = 'new'
+  end
+end
